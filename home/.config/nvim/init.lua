0a3ddc9467c8a6f5b392bce2163ceb0e9f43df6e
Taras Tuz (aka Quantie) <tarastuz@proton.me>
2025-11-23 14:45:41 +0000

    nvim: make `;` keybind work in visual mode too Meow UwU

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -99,6 +99,8 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 vim.keymap.set("n", ":", "q:i")
 vim.keymap.set("n", ";", ":")
+vim.keymap.set("v", ";", ":is/")
+
 vim.keymap.set("v", ":", "q:is/")
 vim.keymap.set("n", "<C-q>", "<cmd>qa<CR>")
 
