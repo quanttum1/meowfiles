06b6c63c3f19cd802faf5732f5f42a96e6d875ad
Taras Tuz (aka Quantie) <tarastuz@proton.me>
2026-02-17 22:36:47 +0000

    sleep-well: show a notification to prepare at 9 pm meow :3

--- home/.config/sway/scripts/sleep-well.py
+++ home/.config/sway/scripts/sleep-well.py
@@ -2,21 +2,31 @@ from datetime import datetime
 import time
 import os
 
+sleep_prepare = '21:00'
 sleep_begin = '21:30'
 sleep_end = '06:30'
 
+sleep_prepare = [int(i) for i in sleep_begin.split(':')]
 sleep_begin = [int(i) for i in sleep_begin.split(':')]
 sleep_end = [int(i) for i in sleep_end.split(':')]
 
-cmd = "swaylock -c 000000"
+shown_prepare = False
 
 while True:
     now = datetime.now()
 
-    sleep_begin_dt = datetime(now.year, now.month, now.day, *sleep_begin)
-    sleep_end_dt = datetime(now.year, now.month, now.day, *sleep_end)
+    sleep_prepare = datetime(now.year, now.month, now.day, *sleep_prepare)
+    sleep_begin = datetime(now.year, now.month, now.day, *sleep_begin)
+    sleep_end = datetime(now.year, now.month, now.day, *sleep_end)
 
-    if now > sleep_begin_dt or now < sleep_end_dt:
+    if now > sleep_prepare and not shown_prepare:
+        os.system('fullscreen-notification "Prepare for sleep" 10')
+        shown_prepare = True
+
+    if now > sleep_begin or now < sleep_end:
         os.system('fullscreen-notification "Go sleep" 10')
+
+    if now > sleep_end and now < sleep_prepare:
+        shown_prepare = False
 
     time.sleep(10)
