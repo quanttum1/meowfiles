fb679211b54a304a2f6df5fc7bd55017dcf192de
Taras Tuz (aka Quantie) <tarastuz@proton.me>
2026-02-22 13:39:55 +0000

    sleep-well: fix it and make it working

--- home/.config/sway/scripts/sleep-well.py
+++ home/.config/sway/scripts/sleep-well.py
@@ -6,7 +6,7 @@ sleep_prepare = '21:00'
 sleep_begin = '21:30'
 sleep_end = '06:30'
 
-sleep_prepare = [int(i) for i in sleep_begin.split(':')]
+sleep_prepare = [int(i) for i in sleep_prepare.split(':')]
 sleep_begin = [int(i) for i in sleep_begin.split(':')]
 sleep_end = [int(i) for i in sleep_end.split(':')]
 
@@ -15,18 +15,18 @@ shown_prepare = False
 while True:
     now = datetime.now()
 
-    sleep_prepare = datetime(now.year, now.month, now.day, *sleep_prepare)
-    sleep_begin = datetime(now.year, now.month, now.day, *sleep_begin)
-    sleep_end = datetime(now.year, now.month, now.day, *sleep_end)
+    sleep_prepare_dt = datetime(now.year, now.month, now.day, *sleep_prepare)
+    sleep_begin_dt = datetime(now.year, now.month, now.day, *sleep_begin)
+    sleep_end_dt = datetime(now.year, now.month, now.day, *sleep_end)
 
-    if now > sleep_prepare and not shown_prepare:
+    if now > sleep_prepare_dt and not shown_prepare:
         os.system('fullscreen-notification "Prepare for sleep" 10')
         shown_prepare = True
 
-    if now > sleep_begin or now < sleep_end:
+    if now > sleep_begin_dt or now < sleep_end_dt:
         os.system('fullscreen-notification "Go sleep" 10')
 
-    if now > sleep_end and now < sleep_prepare:
+    if now > sleep_end_dt and now < sleep_prepare_dt:
         shown_prepare = False
 
     time.sleep(10)
