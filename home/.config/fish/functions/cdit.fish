84272b61abefb0cd3f7bde0cbf494a88ccaad19f
Taras Tuz (aka Quantie) <tarastuz@proton.me>
2026-05-10 22:03:45 +0100

    fish: fix `cdit` function before that it produced `files`

--- home/.config/fish/functions/cdit.fish
+++ home/.config/fish/functions/cdit.fish
@@ -2,11 +2,7 @@ function cdit
     set last_command (history --max=1)
     if test -n "$last_command"
         set -l args (string split ' ' -- $last_command)
-        if count $args > 0
-            cd (string trim -- (string collect -- $args[-1]))
-        else
-            echo "No arguments found in last command"
-        end
+        cd (string trim -- (string collect -- $args[-1]))
     else
         echo "No previous command found"
     end
