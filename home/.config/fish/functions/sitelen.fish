b2266ec424a226b81da07f86d4e065076ce51736
jan Luna Wani <janLunaWani@proton.me>
2026-05-17 14:40:33 +0100

    󱥄󱥔󱤉󱤎󱥍󱥠󱦐󱥯󱥛󱦜󱥖󱦜󱦑 󱥫󱥐󱤡󱥆󱤧󱥌󱤉󱤩󱥸　󱤩󱥁󱤧󱤓󱤉󱤂

--- home/.config/fish/functions/sitelen.fish
+++ home/.config/fish/functions/sitelen.fish
@@ -1,7 +1,7 @@
 function sitelen
     touch /tmp/sitelen.txt
     nvim /tmp/sitelen.txt
-    cat /tmp/sitelen.txt | wl-copy
-    cat /tmp/sitelen.txt | sitelen-ucsur | wl-copy
+    perl -pe 'chomp if eof' /tmp/sitelen.txt | wl-copy
+    perl -pe 'chomp if eof' /tmp/sitelen.txt | sitelen-ucsur | wl-copy
     rm /tmp/sitelen.txt
 end
