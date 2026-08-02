b00c3072414fa3347f4129ff4ea7daf81bed8b3b
jan Luna Wani <janLunaWani@proton.me>
2026-08-02 12:22:04 +0300

    fix the suspend_handler lol

--- home/.suspend_handler.sh
+++ home/.suspend_handler.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# this script is run when i press power button on my laptop
-# or close the lid
-systemd suspend
+# this script is run when i press power button on my laptop or close the lid
+
+systemd-run --user --machine=meow@ swaylock -c 000000
+systemctl suspend
